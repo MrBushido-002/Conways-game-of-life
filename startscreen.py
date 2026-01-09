11c4e31c2fbf2382c5fd923d07258ada9c96ebb2
Connor Watson <connorwatson@Tammis-MacBook-Pro.local>
2026-01-08 20:15:48 -0600

    deleted unused file

--- startscreen.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.font.init()
-text_font = pygame.font.SysFont("Arial", 30)
-
-def draw_text(text, font, x, y, text_col = "BLACK"):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x,y))
